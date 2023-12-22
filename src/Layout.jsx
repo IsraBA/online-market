5e3f2e0b9f20486bfec601c1f25816074ffd60c5
IsraBA <IsraelBenAri1000@gmail.com>
2023-12-22 03:26:50 +0200

    css categories

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -34,28 +34,28 @@ export default function Layout() {
             <div className="home" onClick={() => setCategory([])}>
               <FontAwesomeIcon icon={faHouse} />
               </div>
-            <div className="category" onClick={() => setCategory(fruits)}>
+            <div className="category" id={category == fruits ? 'selected': ""} onClick={() => setCategory(fruits)}>
               <div className="imgHolder">
                 <img src="https://png.pngtree.com/png-clipart/20230310/ourmid/pngtree-fresh-fruit-png-image_6642661.png" alt="Fruits" />
               </div>
               Fruits
             </div>
             <span className='gapLine'></span>
-            <div className="category" onClick={() => setCategory(vegetables)}>
+            <div className="category" id={category == vegetables ? 'selected': ""} onClick={() => setCategory(vegetables)}>
               <div className="imgHolder">
                 <img src="https://static.vecteezy.com/system/resources/previews/022/984/730/non_2x/vegetable-transparent-free-png.png" alt="Vegetables" />
               </div>
               Vegetables
             </div>
             <span className='gapLine'></span>
-            <div className="category" onClick={() => setCategory(alcohol)}>
+            <div className="category" id={category == alcohol ? 'selected': ""} onClick={() => setCategory(alcohol)}>
               <div className="imgHolder">
                 <img src="https://roust.com/f/img/responsive/bottles@1x.png" alt="Alcohol" />
               </div>
               Alcohol
             </div>
             <span className='gapLine'></span>
-            <div className="category" onClick={() => setCategory(dairy)}>
+            <div className="category" id={category == dairy ? 'selected': ""} onClick={() => setCategory(dairy)}>
               <div className="imgHolder">
                 <img src="https://png.monster/wp-content/uploads/2022/06/png.monster-790.png" alt="Dairy" />
               </div>
